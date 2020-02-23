eb184f3d68249d64bf014099fb331fc053518f3d
Yoshi Russell <yoshiichirorussell@gmail.com>
2020-02-22 20:19:04 -0800

    uh

--- functions/index.js
+++ functions/index.js
@@ -33,10 +33,10 @@ exports.webApi = functions.https.onRequest(app);
 // path that triggers this 
 //   = https://us-central1-(...).cloudfunctions.net/webApi/api/v1/session
 app.post('/session', (request, response) => {
-    response.setHeader('Cache-Control', 'private');
-    response.setHeader("Access-Control-Allow-Origin", request.headers.origin);
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, UPDATE, PUT");
-    response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type");
+    response.header('Cache-Control', 'private');
+    response.header("Access-Control-Allow-Origin", "https://quirky-jennings-bbbbeb.netlify.com/");
+    response.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, UPDATE, PUT");
+    response.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type");
     response.header("Access-Control-Allow-Credentials", true);
        
     // check if user has persistent cookie or session cookie
